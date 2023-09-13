2c7285b1431c091bb9abe2d31425e1e8014ee6b7
Marakin IQA-65 <le-ha12@yandex.ru>
2023-09-13 21:05:39 +0300

    New

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.junit.Test
+    public void shouldAddTheAmount() {
+        int amount = 600;
+        int actual = service.remain(amount);
+        int expected = 400;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void mustEqualsZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void mustEqualsOne() {
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void notEnoughToLimit() {
+        int amount = 1400;
+        int actual = service.remain(amount);
+        int expected = 600;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void moreThanOneLimit() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+     @org.junit.Test
+    public void amountEqualToTheBonus() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+}
